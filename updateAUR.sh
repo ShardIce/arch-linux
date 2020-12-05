395b30d5368864ac58d8d25ca7fe0267e8cfaff6
ShardIce <48800677+ShardIce@users.noreply.github.com>
2020-12-05 14:05:23 +0300

    Update updateAUR.sh

--- updateAUR.sh
+++ updateAUR.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 path1=/Desktop/AUR
 
-echo -e "\e[1;37;1;42m СТАРТ \e[0m"
-echo -e "\e[1;Устанавливаем основные пакеты\e[0m"
-#sudo pacman -S samba ktorrent chromium filezilla htop nftables qbittorrent telegram-desktop transmission-cli wine-gecko wine-mono ttf-dejavu
-
 echo "Идем в директорию"
 cd $PWD$path1
 echo $PWD
