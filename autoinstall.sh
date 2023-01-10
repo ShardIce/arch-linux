0edd1881ee36f2412c8cb587673b2fe28c6da4a5
ShardIce <48800677+ShardIce@users.noreply.github.com>
2023-01-10 08:55:15 -0800

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -38,10 +38,10 @@ pacman-key --populate archlinux
 pacman -Sy
 
 # Разметка диска
-printf -e "g\nw\n" | fdisk /dev/sda # создаём gpt
-printf -e "n\n1\n\n+1G\nt\n2\n4\nw\n" | fdisk /dev/sda # первый раздел 1Гб
-printf -e "n\n2\n\n+10G\n2\nw\n" | fdisk /dev/sda # второй раздел 10Гб
-printf -e "n\n3\n\n\nw\n" | fdisk /dev/sda # третий раздел - остаток
+printf "g\nw\n" | fdisk /dev/sda # создаём gpt
+printf "n\n1\n\n+1G\nt\n2\n4\nw\n" | fdisk /dev/sda # первый раздел 1Гб
+printf "n\n2\n\n+10G\n2\nw\n" | fdisk /dev/sda # второй раздел 10Гб
+printf "n\n3\n\n\nw\n" | fdisk /dev/sda # третий раздел - остаток
 
  
 #Форматируем в ext 4 наш диск
