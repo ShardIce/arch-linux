67e7f5562306ad057ac15953ee73b0ba05a108fa
ShardIce <48800677+ShardIce@users.noreply.github.com>
2023-01-20 19:38:31 -0800

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -40,10 +40,11 @@ pacman-key --init
 pacman-key --populate archlinux
 
 echo "Разметка диска"
-printf "g\nw\n" | fdisk /dev/sda # создаём gpt
-printf "n\n1\n\n+1G\nt\n4\nw\n" | fdisk -L /dev/sda # первый раздел 1Гб
-printf "n\n2\n\n+10G\nt\n2\n19\nw\n" | fdisk -L /dev/sda # второй раздел 10Гб
-printf "n\n3\n\n\nw\n" | fdisk -L /dev/sda # третий раздел - остаток
+echo -e 'bootable size=1G, type=swap\n size=10G, type=L\n size=+\n' | sfdisk /dev/sda
+#printf "g\nw\n" | fdisk /dev/sda # создаём gpt
+#printf "n\n1\n\n+1G\nt\n4\nw\n" | fdisk -L /dev/sda # первый раздел 1Гб
+#printf "n\n2\n\n+10G\nt\n2\n19\nw\n" | fdisk -L /dev/sda # второй раздел 10Гб
+#printf "n\n3\n\n\nw\n" | fdisk -L /dev/sda # третий раздел - остаток
  
 echo "Форматируем в ext 4 наш диск"
 mkfs.ext4 /dev/sda1
