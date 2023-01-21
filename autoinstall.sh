9189a1a79ccaa9f237a800a59339ac36ef596402
ShardIce <48800677+ShardIce@users.noreply.github.com>
2023-01-20 20:23:42 -0800

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -40,7 +40,7 @@ pacman-key --init
 pacman-key --populate archlinux
 
 echo "Разметка диска"
-echo -e 'size=1G, type=bootable\n,size=10G,type=swap\n, type=L\n size=+\n' | sfdisk /dev/sda
+echo -e 'type=bootable\n size=1G, type=swap\n size=10G, type=L\n size=+\n' | sfdisk /dev/sda
 #printf "g\nw\n" | fdisk /dev/sda # создаём gpt
 #printf "n\n1\n\n+1G\nt\n4\nw\n" | fdisk -L /dev/sda # первый раздел 1Гб
 #printf "n\n2\n\n+10G\nt\n2\n19\nw\n" | fdisk -L /dev/sda # второй раздел 10Гб
