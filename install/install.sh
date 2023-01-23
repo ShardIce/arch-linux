daa7bfb348fcc75f127c147eed41eebadd18ba33
ShardIce <48800677+ShardIce@users.noreply.github.com>
2023-01-23 13:18:46 -0800

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -7,9 +7,7 @@ curl -O https://raw.githubusercontent.com/ShardIce/arch-linux/master/install/set
 ./settings
 echo "Подтянули настройки для $USERNAME"
 
-# Указываем названия пакетов с двойными ковычками
-for var in "mirror.surf mirror.nw-sys.ru mirrors.powernet.com.ru mirror.rol.ru mirror.truenetwork.ru mirror.yandex.ru mirror.23media.com"
-
+# Servers
 do
 echo -e "\e[1;37;1;42m Заходим на сервер $var \e[0m"
 git clone https://$var/archlinux/\$repo/os/\$arch
