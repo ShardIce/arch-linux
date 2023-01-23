bcd64cada2178cccc1c076acb88afde2f07217df
ShardIce <48800677+ShardIce@users.noreply.github.com>
2023-01-23 13:17:32 -0800

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -6,3 +6,11 @@ curl -O https://raw.githubusercontent.com/ShardIce/arch-linux/master/install/set
 
 ./settings
 echo "Подтянули настройки для $USERNAME"
+
+# Указываем названия пакетов с двойными ковычками
+for var in "mirror.surf mirror.nw-sys.ru mirrors.powernet.com.ru mirror.rol.ru mirror.truenetwork.ru mirror.yandex.ru mirror.23media.com"
+
+do
+echo -e "\e[1;37;1;42m Заходим на сервер $var \e[0m"
+git clone https://$var/archlinux/\$repo/os/\$arch
+echo "Показываем на каком мы сервере $var\n"
