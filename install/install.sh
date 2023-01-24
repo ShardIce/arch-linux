f3866fdf2984ed23db803f8e20170bdbb9860d14
ShardIce <48800677+ShardIce@users.noreply.github.com>
2023-01-23 23:54:33 -0800

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -3,7 +3,6 @@
 set +x
 
 curl -O https://raw.githubusercontent.com/ShardIce/arch-linux/master/install/settings
-chmod 0775 setings
 
 . settings
 echo "Подтянули настройки для $USERNAME"
@@ -14,4 +13,3 @@ echo -e "\e[1;37;1;42m Сохраняем актуальный mirrorlist \e[0m"
 sudo curl -o /etc/pacman.d/mirrorlist https://archlinux.org/mirrorlist/?country=$COUNTRY&protocol=$PROTOCOL&ip_version=$IP_VERSION
 sleep 5
 sudo sed -i -e "s/#Server\ /Server\ /g" /etc/pacman.d/mirrorlist
-
