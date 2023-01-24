d05a5ca65657542d8471e3e679c73b0b11bcc0cd
ShardIce <48800677+ShardIce@users.noreply.github.com>
2023-01-24 02:47:33 -0800

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -12,7 +12,6 @@ echo "Ваша версия IP протокола: " $IP_VERSION
 read -e -p "Введите протокол (http) или (https): " PROTOCOL
 echo "Ваша версия протокола:" $PROTOCOL
 
-
 cat <<SET>./settings
 #!/bin/bash
 
@@ -24,14 +23,13 @@ IP_VERSION=$IP_VERSION
 PROTOCOL=$PROTOCOL
 SET
 
-#curl -O https://raw.githubusercontent.com/ShardIce/arch-linux/master/install/settings
-
 . settings
 echo "Подтянули настройки для $USERNAME"
 
 echo -e "\e[1;37;1;42m Сохраняем актуальный mirrorlist \e[0m"
 # Servers
 # Указываем названия серверов
-sudo curl -o /etc/pacman.d/mirrorlist https://archlinux.org/mirrorlist/?country=$COUNTRY&protocol=$PROTOCOL&ip_version=$IP_VERSION
+
+curl -o /etc/pacman.d/mirrorlist https://archlinux.org/mirrorlist/?country=$COUNTRY&protocol=$PROTOCOL&ip_version=$IP_VERSION
 sleep 5
-sudo sed -i -e "s/#Server\ /Server\ /g" /etc/pacman.d/mirrorlist
+sed -i -e "s/#Server\ /Server\ /g" /etc/pacman.d/mirrorlist
