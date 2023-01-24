8109b3e7d430ef047abaebadd993b1e399b4c235
ShardIce <48800677+ShardIce@users.noreply.github.com>
2023-01-23 22:24:35 -0800

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -11,5 +11,7 @@ echo "Подтянули настройки для $USERNAME"
 echo -e "\e[1;37;1;42m Сохраняем актуальный mirrorlist \e[0m"
 # Servers
 # Указываем названия серверов
-curl -o /home/$USERNAME/Desktop/mirrorlist https://archlinux.org/mirrorlist/?country=$COUNTRY&protocol=$PROTOCOL&ip_version=$IP_VERSION
+sudo curl -o /etc/pacman.d/mirrorlist https://archlinux.org/mirrorlist/?country=$COUNTRY&protocol=$PROTOCOL&ip_version=$IP_VERSION
+sleep 5
+sudo sed -i -e "s/#Server\ /Server\ /g" /etc/pacman.d/mirrorlist
 
